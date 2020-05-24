bd2352914f0f64f5292a84d72501aba523b3b4b6
7rs <rsytav@gmail.com>
2020-05-25 02:55:27 +0900

    各種アカウントに飛べるようにしてみる

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,20 +1,8 @@
 <template>
   <v-app dark>
-    <v-navigation-drawer
-      v-model="drawer"
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      fixed
-      app
-    >
+    <v-navigation-drawer v-model="drawer" :mini-variant="miniVariant" :clipped="clipped" fixed app>
       <v-list>
-        <v-list-item
-          v-for="(item, i) in items"
-          :key="i"
-          :to="item.to"
-          router
-          exact
-        >
+        <v-list-item v-for="(item, i) in items" :key="i" :to="item.to" router exact>
           <v-list-item-action>
             <v-icon>{{ item.icon }}</v-icon>
           </v-list-item-action>
@@ -24,16 +12,10 @@
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
-    <v-app-bar
-      :clipped-left="clipped"
-      fixed
-      app
-    >
+    <v-app-bar :clipped-left="clipped" fixed app>
       <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-btn
-        icon
-        @click.stop="miniVariant = !miniVariant"
-      >
+      <!--
+      <v-btn icon @click.stop="miniVariant = !miniVariant">
         <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
       </v-btn>
       <v-btn
@@ -42,6 +24,7 @@
       >
         <v-icon>mdi-application</v-icon>
       </v-btn>
+
       <v-btn
         icon
         @click.stop="fixed = !fixed"
@@ -56,12 +39,14 @@
       >
         <v-icon>mdi-menu</v-icon>
       </v-btn>
+      -->
     </v-app-bar>
     <v-content>
       <v-container>
         <nuxt />
       </v-container>
     </v-content>
+    <!--
     <v-navigation-drawer
       v-model="rightDrawer"
       :right="right"
@@ -85,33 +70,29 @@
     >
       <span>&copy; {{ new Date().getFullYear() }}</span>
     </v-footer>
+    -->
   </v-app>
 </template>
 
 <script>
 export default {
-  data () {
+  data() {
     return {
+      title: "7RS",
       clipped: false,
       drawer: false,
       fixed: false,
-      items: [
-        {
-          icon: 'mdi-apps',
-          title: 'Welcome',
-          to: '/'
-        },
-        {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire'
-        }
-      ],
-      miniVariant: false,
+      miniVariant: true,
       right: true,
       rightDrawer: false,
-      title: 'Vuetify.js'
-    }
+      items: [
+        {
+          icon: "mdi-apps",
+          title: "Welcome",
+          to: "/"
+        }
+      ]
+    };
   }
-}
+};
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,92 +1,49 @@
 <template>
-  <v-layout
-    column
-    justify-center
-    align-center
-  >
-    <v-flex
-      xs12
-      sm8
-      md6
-    >
-      <div class="text-center">
-        <logo />
-        <vuetify-logo />
-      </div>
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
-        </v-card-title>
-        <v-card-text>
-          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
-          <p>
-            For more information on Vuetify, check out the <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-            >
-              documentation
-            </a>.
-          </p>
-          <p>
-            If you have questions, please join the official <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              title="chat"
-            >
-              discord
-            </a>.
-          </p>
-          <p>
-            Find a bug? Report it on the github <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              title="contribute"
-            >
-              issue board
-            </a>.
-          </p>
-          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3">
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-          >
-            Nuxt Documentation
-          </a>
-          <br>
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-          >
-            Nuxt GitHub
-          </a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn
-            color="primary"
-            nuxt
-            to="/inspire"
-          >
-            Continue
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-flex>
-  </v-layout>
+  <div>
+    <h1>Hello, world!</h1>
+    <v-banner v-for="item in items" :key="item.title" single-line>
+      <v-avatar class="mx-3">
+        <img :src="item.img" :alt="item.title" />
+      </v-avatar>
+      {{item.name}}
+      <template v-slot:actions>
+        <v-btn class="align-center" text :href="item.url">SHOW</v-btn>
+      </template>
+    </v-banner>
+  </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
-
 export default {
-  components: {
-    Logo,
-    VuetifyLogo
-  }
-}
+  head: {
+    title: "Top"
+  },
+  methods: {
+    alert() {
+      alert("Hello, World!");
+    }
+  },
+  data: () => ({
+    items: [
+      {
+        img: require("~/assets/images/twitter.png"),
+        name: "@R4XELN",
+        url: "https://twitter.com/R4XELN",
+        title: "Twitter"
+      },
+      {
+        img: require("~/assets/images/instagram.png"),
+        name: "@R4zLn",
+        url: "https://instagram.com/R4zLn",
+        title: "Instagram"
+      },
+      {
+        img: require("~/assets/images/github.png"),
+        name: "@7rs",
+        url: "https://github.com/7rs",
+        title: "GitHub"
+      }
+    ]
+  })
+};
 </script>
