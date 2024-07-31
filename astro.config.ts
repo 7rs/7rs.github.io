fe744cd1327c6fc97e6e9a99dc54c3c0d98bdc50
Cbrnex <31788262+7rs@users.noreply.github.com>
2024-08-01 00:04:24 +0900

    add dependencies

--- astro.config.ts
+++ astro.config.ts
@@ -1,16 +1,23 @@
 import path from 'node:path';
 import { defineConfig } from 'astro/config';
+
+import svelte from '@astrojs/svelte';
+import mdx from '@astrojs/mdx';
+import icon from 'astro-icon';
+import UnoCSS from 'unocss/astro';
+import compressor from 'astro-compressor';
+import playformCompress from '@playform/compress';
+import purgecss from 'astro-purgecss';
+
 import tsconfig from './tsconfig.json';
-import svelte from "@astrojs/svelte";
-let aliasesArray: {
-  [key: string]: string;
-} = {};
+
+let aliasesArray: { [key: string]: string } = {};
 for (const [alias, targetPath] of Object.entries(tsconfig.compilerOptions.paths)) {
   aliasesArray = {
     ...aliasesArray,
     ...{
-      [alias]: path.resolve(targetPath[0])
-    }
+      [alias]: path.resolve(targetPath[0]),
+    },
   };
 }
 
@@ -18,14 +25,14 @@ for (const [alias, targetPath] of Object.entries(tsconfig.compilerOptions.paths)
 export default defineConfig({
   vite: {
     resolve: {
-      alias: aliasesArray
-    }
+      alias: aliasesArray,
+    },
   },
   markdown: {
     gfm: false,
     shikiConfig: {
-      theme: 'dracula'
-    }
+      theme: 'dracula',
+    },
   },
-  integrations: [svelte()]
+  integrations: [icon(), mdx(), svelte(), UnoCSS(), purgecss(), playformCompress(), compressor()],
 });
